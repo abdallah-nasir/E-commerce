f4b91c8f2af67576b93faae7453d3dc4ac1937dc
abdallah-nasir <abdallah.nasir@ymail.com>
2021-06-04 16:10:53 +0300

    deploy/6-4*4

--- E_commerce/urls.py
+++ E_commerce/urls.py
@@ -24,16 +24,15 @@ from django.utils.translation import gettext_lazy as _
 # from allauth.account.url
 
 
-urlpatterns = i18n_patterns(
-    path(_('admin/'), admin.site.urls),
-    path(_('accounts/'), include('allauth.urls')),
+urlpatterns = [
+    path('admin/'), admin.site.urls,
+    path('accounts/'), include('allauth.urls'),
 
     path("",include("shop.urls",namespace="shop")),
+]
+urlpatterns += i18n_patterns(
+    path("",include("shop.urls",namespace="shop")),
 )
-
-# urlpatterns += i18n_patterns(
-#     path("",include("shop.urls",namespace="shop")),
-# )
 
 
 if settings.DEBUG:
