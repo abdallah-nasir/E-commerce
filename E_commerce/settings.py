fe407205f90555f18e8c9a768ed7d4217c22e1af
abdallah-nasir <abdallah.nasir@ymail.com>
2021-06-04 15:54:19 +0300

    deploy/6-4*2

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY=config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = False 
 TEMPLATE_DEBUG = DEBUG
 ALLOWED_HOSTS =["127.0.0.1","localhost","universal-e-commerce.herokuapp"]
 SESSION_COOKIE_HTTPONLY = True
